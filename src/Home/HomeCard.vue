9d8362af6f9370a03557d7a97c2c06330b1d265e
lungaHamilton09 <141244743+lungaHamilton09@users.noreply.github.com>
2024-02-21 13:35:00 +0200

    Added styling onto the about and experience

--- src/Home/HomeCard.vue
+++ src/Home/HomeCard.vue
@@ -62,42 +62,89 @@
   <div class="about-container">
 
     <div class="about-image-container">
-      <img src="../images/IMG_7093.jpg" alt="#" class="hero-image">
+      <img src="../images/IMG_7093.jpg" alt="#" class="about-image">
     </div>
 
     <div class="about-text-container">
-      <h1>Heading</h1>
-  
-      <div class="experience-boxes">
-      <div>Experience</div>
-      <a href="">
-        <img src="../images/Experience-icon.png" alt="experience" class="experience-icon">
-      </a>
-      <p>6+ Months
-         Frontend 
-         Development.</p>
+      <h1>About Me</h1>
 
-      <div>Education</div>
-      <a href="">
-        <img src="../images/Education-icon.png" alt="education" class="education-icon">
-      </a>
-      <p> BCOM. 
-          Bachelors 
-          Degree.</p>
+      <div class="about-row">
+        <div class="experience-box">
+          <div>
+            <h1>Experience</h1>
+            <p>
+              6+ Months <br>
+              Frontend <br>
+              Development. <br>
+            </p>
+          </div>
+          <div>
+            <a href="">
+              <img src="../images/Experience-icon.png" alt="experience" class="experience-icon">
+            </a>
+          </div>
+        </div>
+
+        <div class="education-box">
+          <div>
+            <h1>Education</h1>
+            <p> 
+              BCOM. <br>
+              Bachelors <br>
+              Degree. <br>
+            </p>
+          </div>
+          <div>
+            <a href="">
+              <img src="../images/Education-icon.png" alt="education" class="education-icon">
+            </a>
+          </div>
+        </div>
       </div>
-      
-      <p>As a highly motivated and dynamic software developer, I am deeply commited to crafting high-end technological solutions and innovative software. My passion lies in creating user-interactive applications that significantly elevate customer experiences. Eager to contribute to the rapid growth of the technological landscape, I aspire to be an integral part of teams driving innovation and excellence in the field of tech and software</p>
+
+      <p>As a highly motivated and dynamic software developer, 
+         I am deeply commited to crafting high-end technological solutions and innovative software. 
+         My passion lies in creating user-interactive applications that significantly elevate customer experiences. 
+         Eager to contribute to the rapid growth of the technological landscape, I aspire to be an integral part of teams driving innovation and 
+         excellence in the field of tech and software</p>
     </div>
   </div>
 
   <!-- experience section -->
   <div class="experience-container">
+    <h1>
+      Explore My 
+      <br> 
+      Experience
+    </h1>
 
+    <div class="experience-image-container">
+      <img src="../images/New Developer icon.jpg" alt="developer-logo" class="developer-icon">
+        <!-- floating icons -->
+    </div>
   </div>
 
   <!-- projects section -->
   <div class="projects-container">
-
+    <h1>Browse My Projects</h1>
+    <h1>Projects</h1>
+    <div class="project-row">
+      <div class="card">
+        <img src="../images/banking app.jpg" alt="" class="card-img">
+        <br>
+        <h1>Banking App</h1>
+      </div>
+      <div class="card">
+        <img src="../images/healthcare Image icon.png" alt="" class="card-img">
+        <br>
+        <h1>Healthcare Website</h1>
+      </div>
+      <div class="card">
+        <img src="../images/Tetris Game.png" alt="" class="card-img">
+        <br>
+        <h1>Tetris Game App</h1>
+      </div>
+    </div>
   </div>
 
   <!-- contact section -->
@@ -125,6 +172,7 @@ export default {
   position: fixed;
   background: #664229;
   padding: 0 3rem;
+  z-index: 2;
 }
 .nav-links {
   display: flex;
@@ -166,8 +214,7 @@ export default {
   height: 100%;
 }
 .hero-image {
-  width: 60%;
-  height: 60%;
+  width: 350px;
   border-radius: 50%;
 }
 .home-left-container {
@@ -228,28 +275,118 @@ export default {
 }
 
 /* about section */
+
 .about-container {
-display: flex;
-flex-direction: row;
-width: 100%;
-height: 100vh;
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  min-width: 100vw;
+  height: 100vh;
+  background-color: #FFF5E2;
 }
-.hero-image {
-  width: 60%;
-  height: 60%;
-  border-radius: 50%;  
+.about-image {
+  border-radius: 50%;
+  width: 350px;
+  height:440px;
+}
+.about-image-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  max-width: 35vw;
 }
 .about-text-container {
   display: flex;
   flex-direction: column;
-  align-items: center;
-  justify-content: flex-start;
-  gap: 3rem;
-
+  align-items: flex-start;
+  justify-content: center;
+  gap: 2rem;
+  width: 100%;
+  max-width: 65vw;
+  padding: 2rem;
+}
+.about-row {
+  display: flex;
+  width: 100%;
+  gap: 1rem;
+  color: #FFF5E2;
+}
+.experience-box, 
+.education-box {
+  min-width: 50%;
+  display: flex;
+  gap: 2rem;
+  padding: 2rem;
+  background-color: #664229;
+}
+.education-icon,
+.experience-icon {
+  width: 100px;
 }
 
+/* experience section */
 
+.experience-container {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  min-width: 100vw;
+  height: 100vh;
+  gap: 2rem;
+  background-color: #664229;
+}
+.experience-image-container {
+  width: fit-content;
+  height: fit-content;
+}
+.developer-icon {
+  width: 100%;
+  min-width: 750px;
+}
 
+/* projects section */
+
+.projects-container {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  min-width: 100vw;
+  height: 100vh;
+  gap: 2rem;
+  background-color: #FFF5E2;
+}
+.project-row {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  min-width: 100vw;
+  height: auto;
+  gap: 3rem;
+}
+.card {
+  padding-top: 10px;
+  border-radius: 5%;
+  text-align: center;
+  width: 360px;
+  height: 320px;
+  /* outline: #664229 1px solid; */
+  background-color: #c98c60;
+}
+.card-img {
+  width: fit-content;
+  max-width: 340px;
+  height: 260px;
+
+}
 
 /* font styles */
 .font-text-small {
